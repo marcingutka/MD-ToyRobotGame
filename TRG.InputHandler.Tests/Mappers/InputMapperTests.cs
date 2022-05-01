66cbc725e13a74f26754a27d3c5861570b357c34
Marcin Gutka <gucios17@gmail.com>
2022-05-01 21:55:00 +0200

    Added test for mapper to check if the validator is called

--- TRG.InputHandler.Tests/Mappers/InputMapperTests.cs
+++ TRG.InputHandler.Tests/Mappers/InputMapperTests.cs
@@ -248,5 +248,25 @@ namespace TRG.InputHandler.Tests.Mappers
             //Assert
             Assert.IsTrue(commandList.Count == 0);
         }
+
+        [Test]
+        public void Map_When_CommandType_Is_PlaceRobot_PlacingValidator_Is_Called_Once()
+        {
+            //Arrange
+            var grid = new Grid(5, 5);
+            var content = new List<string>();
+
+            var command = "PLACE_ROBOT 2,3,CENTER";
+
+            content.Add(command);
+
+            validator.Validate(default, default, default).ReturnsForAnyArgs(false);
+
+            //Act
+            mapper.Map(content, grid);
+
+            //Assert
+            Assert.DoesNotThrow(() => validator.Received(1).Validate(Arg.Any<Grid>(), Arg.Any<int>(), Arg.Any<int>()));
+        }
     }
 }
