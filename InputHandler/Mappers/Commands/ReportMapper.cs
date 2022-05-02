912302ad1ceb7824d24d1d1e237d42c32cff1e45
Marcin Gutka <gucios17@gmail.com>
2022-05-02 18:09:12 +0200

    Added REPORT mapper

---
+++ InputHandler/Mappers/Commands/ReportMapper.cs
@@ -0,0 +1,10 @@
+﻿using TRG.Models.Commands;
+
+namespace TRG.InputHandler.Mappers.Commands
+{
+    internal class ReportMapper : IMapCommand
+    {
+        public Command Map(List<string> parameters) =>
+            parameters == null ? new Report() : null;
+    }
+}
