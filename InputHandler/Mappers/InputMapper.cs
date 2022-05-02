fec364c7ad71de97599e858767149f87a520a9b3
Marcin Gutka <gucios17@gmail.com>
2022-05-02 18:14:36 +0200

    Removed unused code

--- InputHandler/Mappers/InputMapper.cs
+++ InputHandler/Mappers/InputMapper.cs
@@ -1,6 +1,4 @@
-﻿using TRG.InputHandler.Conts;
-using TRG.Models.Commands;
-using TRG.Models.Enums;
+﻿using TRG.Models.Commands;
 using TRG.Models.Model;
 
 namespace TRG.InputHandler.Mappers
@@ -35,74 +33,9 @@ namespace TRG.InputHandler.Mappers
                 {
                     commandList.Add(mappedCommand);
                 }
-
-                /*try
-                {
-                    var commandType = MapCommandType(commandName);
-
-                    if (commandType == CommandType.Movement)
-                    {
-                        commandList.Add(MapMovementCommand(commandName));
-                    }
-                    else if (commandType == CommandType.PlaceRobot || commandType == CommandType.PlaceWall)
-                    {
-                        var commandParameters = splitedCommand[1].Split(COMMAND_PARAMETERS_SEPARATOR).ToList();
-
-                        if (commandParameters.Count >= 2 && placingValidator.Validate(grid, int.Parse(commandParameters[0]), int.Parse(commandParameters[1])))
-                        {
-                            commandList.Add(MapCommand(commandType, commandParameters));
-                        }
-                    }
-                    else
-                    {
-                        commandList.Add(MapCommand(commandType, null));
-                    }
-                }
-                catch (NotSupportedException) 
-                { }*/
             }
 
             return commandList;
         }
-
-        private static CommandType MapCommandType(string commandType) => 
-            commandType switch
-            {
-                AllowedCommands.PLACE_ROBOT => CommandType.PlaceRobot,
-                AllowedCommands.PLACE_WALL => CommandType.PlaceWall,
-                AllowedCommands.REPORT => CommandType.Report,
-                AllowedCommands.MOVE => CommandType.Movement,
-                AllowedCommands.LEFT => CommandType.Movement,
-                AllowedCommands.RIGHT => CommandType.Movement,
-                _ => throw new NotSupportedException()
-            };
-
-        private static Command MapMovementCommand(string moveCommand) => 
-            moveCommand switch
-            {
-                AllowedCommands.LEFT => new Movement(MovementCommand.Left),
-                AllowedCommands.RIGHT => new Movement(MovementCommand.Right),
-                AllowedCommands.MOVE => new Movement(MovementCommand.Forward),
-                _ => throw new NotSupportedException()
-            };
-
-        private static Command MapCommand(CommandType commandType, List<string> commandParameters) =>
-            commandType switch
-            {
-                CommandType.PlaceRobot => new PlaceRobot(new GridPosition { Y = int.Parse(commandParameters[0]), X = int.Parse(commandParameters[1]), Orientation = MapFacing(commandParameters[2]) }),
-                CommandType.PlaceWall => new PlaceWall(new Position { Y = int.Parse(commandParameters[0]), X = int.Parse(commandParameters[1]) }),
-                CommandType.Report => new Report(),
-                _ => throw new NotSupportedException()
-            };
-
-        private static OrientationState MapFacing(string facing) =>
-            facing switch
-            {
-                AllowedFacings.NORTH => OrientationState.North,
-                AllowedFacings.EAST => OrientationState.East,
-                AllowedFacings.SOUTH => OrientationState.South,
-                AllowedFacings.WEST => OrientationState.West,
-                _ => throw new NotSupportedException()
-            };
     }
 }
