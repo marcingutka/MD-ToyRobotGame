75e29cc76c435ab1484cf02b39c565752b134b6e
Marcin Gutka <gucios17@gmail.com>
2022-05-03 09:52:10 +0200

    Added tests for REPORT and ExecuteCommand

--- TRG.Logic.Tests/Helpers/CreatorHelper.cs
+++ TRG.Logic.Tests/Helpers/CreatorHelper.cs
@@ -1,14 +1,18 @@
-﻿using TRG.Models.Enums;
+﻿using TRG.Models.Commands;
+using TRG.Models.Enums;
 using TRG.Models.Model;
 
 namespace TRG.Logic.Tests.Helpers
 {
     internal static class CreatorHelper
     {
-       internal static Robot CreateRobot(int x, int y, OrientationState orientation) =>
-            new() { Position = new GridPosition { X = x, Y = y, Orientation = orientation } };
+        internal static Robot CreateRobot(int x, int y, OrientationState orientation) =>
+             new() { Position = new GridPosition { X = x, Y = y, Orientation = orientation } };
 
         internal static GridPoint CreateGridPoint(int x, int y, bool isWall) =>
             new() { Position = new GridPosition { X = x, Y = y }, IsWall = isWall };
+
+        internal static PlaceRobot CreatePlaceRobotCommand(int x, int y, OrientationState orientation) =>
+             new( new GridPosition { X = x, Y = y, Orientation = orientation } );
     }
 }
---
+++ TRG.Logic.Tests/Manager/GameManagerTests.cs
@@ -0,0 +1,69 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TRG.Logic.Manager;
+using TRG.Logic.Tests.Helpers;
+using TRG.Models.Commands;
+using TRG.Models.Model;
+
+namespace TRG.Logic.Tests.Manager
+{
+    public class GameManagerTests
+    {
+        private ICommandManager commandManager;
+        private IGameManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            commandManager = Substitute.For<ICommandManager>();
+            manager = new GameManager(commandManager);
+        }
+
+        [Test]
+        public void ExecuteCommand_WHEN_Command_Is_Report_And_Robot_Exist_Returns_Robot_Position()
+        {
+            //Arrange
+            var grid = new Grid(5, 5);
+            var placeRobotCommand = CreatorHelper.CreatePlaceRobotCommand(1, 1, Models.Enums.OrientationState.North);
+            var command = new Report();
+
+            manager.ConfigureManager(grid);
+
+            commandManager.ExecuteCommand(ref Arg.Any<Robot>(), ref Arg.Any<List<GridPoint>>(), Arg.Is<Command>(x => x is PlaceRobot), grid)
+                .Returns(x =>
+                { 
+                    x[0] = CreatorHelper.CreateRobot(1, 1, Models.Enums.OrientationState.North);
+                    return string.Empty;
+                });
+
+            commandManager.ExecuteCommand(ref Arg.Is<Robot>(x => x.Position.X == 1 && x.Position.Y == 1 && x.Position.Orientation == Models.Enums.OrientationState.North), ref Arg.Any<List<GridPoint>>(), Arg.Is<Command>(x => x is Report), grid)
+                .Returns("1,1,NORTH");
+
+            //Act
+            manager.ExecuteCommand(placeRobotCommand);
+            var result = manager.ExecuteCommand(command);
+
+            //Assert
+            Assert.AreEqual("1,1,NORTH", result);
+        }
+
+        [Test]
+        public void ExecuteCommand_WHEN_Command_Is_Report_And_Robot_DO_NOT_Exist_Returns_Empty_String()
+        {
+            //Arrange
+            var grid = new Grid(5, 5);
+            var command = new Report();
+
+            manager.ConfigureManager(grid);
+
+            commandManager.ExecuteCommand(ref Arg.Is<Robot>(x => x == null), ref Arg.Any<List<GridPoint>>(), Arg.Is<Command>(x => x is Report), grid).Returns(string.Empty);
+
+            //Act
+            var result = manager.ExecuteCommand(command);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(result));
+        }
+    }
+}
