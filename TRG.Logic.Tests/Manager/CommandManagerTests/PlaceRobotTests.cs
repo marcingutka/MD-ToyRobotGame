9245806356daa2bb860cccf6f90f84437acb1001
Marcin Gutka <gucios17@gmail.com>
2022-05-03 10:06:12 +0200

    Changed Robot Place logic according to the requirements

--- TRG.Logic.Tests/Manager/CommandManagerTests/PlaceRobotTests.cs
+++ TRG.Logic.Tests/Manager/CommandManagerTests/PlaceRobotTests.cs
@@ -20,6 +20,42 @@ namespace TRG.Logic.Tests.Manager.CommandManagerTests
         }
 
         [Test]
+        public void Execute_WHEN_PlaceRobot_Command_AND_There_Is_No_Robot_AND_Point_Do_Not_Have_Wall_SET_Robot_Position_To_Provided_State()
+        {
+            //Arrange
+            var grid = new Grid(5, 5);
+            List<GridPoint> gridPoints = new() { CreatorHelper.CreateGridPoint(2, 2, true) };
+            Robot robot = null;
+            var command = new PlaceRobot(new GridPosition { X = 1, Y = 2, Orientation = OrientationState.North });
+
+            //Act
+            manager.ExecuteCommand(ref robot, ref gridPoints, command, grid);
+
+            //Assert
+            Assert.AreEqual(1, robot.Position.X);
+            Assert.AreEqual(2, robot.Position.Y);
+            Assert.AreEqual(OrientationState.North, robot.Position.Orientation);
+        }
+
+        [Test]
+        public void Execute_WHEN_PlaceRobot_Command_AND_There_Is_Robot_AND_Point_Do_Not_Have_Wall_SET_Robot_Position_To_Provided_State()
+        {
+            //Arrange
+            var grid = new Grid(5, 5);
+            List<GridPoint> gridPoints = new() { CreatorHelper.CreateGridPoint(2, 2, true) };
+            Robot robot = CreatorHelper.CreateRobot(4, 3, OrientationState.South);
+            var command = new PlaceRobot(new GridPosition { X = 1, Y = 2, Orientation = OrientationState.North });
+
+            //Act
+            manager.ExecuteCommand(ref robot, ref gridPoints, command, grid);
+
+            //Assert
+            Assert.AreEqual(1, robot.Position.X);
+            Assert.AreEqual(2, robot.Position.Y);
+            Assert.AreEqual(OrientationState.North, robot.Position.Orientation);
+        }
+
+        [Test]
         public void Execute_WHEN_PlaceRobot_Command_AND_There_Is_No_Robot_AND_Point_Has_Wall_DO_NOT_Change_Robot_Position()
         {
             //Arrange
@@ -83,44 +119,6 @@ namespace TRG.Logic.Tests.Manager.CommandManagerTests
 
             //Assert
             Assert.AreEqual(Responses.COORDINATES_ARE_OCCUPIED_BY_WALL, result);
-        }
-
-
-        //Following 2 tests are replacing above 2 tests -> depends on requirements
-        [Test]
-        public void Execute_WHEN_PlaceRobot_Command_AND_There_Is_No_Robot_AND_Point_Has_Wall_SET_Robot_Position_To_Provided_State()
-        {
-            //Arrange
-            var grid = new Grid(5, 5);
-            List<GridPoint> gridPoints = new() { CreatorHelper.CreateGridPoint(2, 2, true) };
-            Robot robot = null;
-            var command = new PlaceRobot(new GridPosition { X = 2, Y = 2, Orientation = OrientationState.North });
-
-            //Act
-            manager.ExecuteCommand(ref robot, ref gridPoints, command, grid);
-
-            //Assert
-            Assert.AreEqual(2, robot.Position.X);
-            Assert.AreEqual(2, robot.Position.Y);
-            Assert.AreEqual(OrientationState.North, robot.Position.Orientation);
-        }
-
-        [Test]
-        public void Execute_WHEN_PlaceRobot_Command_AND_There_Is_Robot_AND_Point_Has_Wall_SET_Robot_Position_To_Provided_State()
-        {
-            //Arrange
-            var grid = new Grid(5, 5);
-            List<GridPoint> gridPoints = new() { CreatorHelper.CreateGridPoint(2, 2, true) };
-            Robot robot = CreatorHelper.CreateRobot(1, 2, OrientationState.South);
-            var command = new PlaceRobot(new GridPosition { X = 2, Y = 2, Orientation = OrientationState.North });
-
-            //Act
-            manager.ExecuteCommand(ref robot, ref gridPoints, command, grid);
-
-            //Assert
-            Assert.AreEqual(2, robot.Position.X);
-            Assert.AreEqual(2, robot.Position.Y);
-            Assert.AreEqual(OrientationState.North, robot.Position.Orientation);
         }
     }
 }
--- TRG.Logic/Services/Executers/PlaceRobotExecuter.cs
+++ TRG.Logic/Services/Executers/PlaceRobotExecuter.cs
@@ -1,4 +1,5 @@
-﻿using TRG.Models.Commands;
+﻿using TRG.Logic.Messages;
+using TRG.Models.Commands;
 using TRG.Models.Model;
 
 namespace TRG.Logic.Services.Executers
@@ -9,10 +10,16 @@ namespace TRG.Logic.Services.Executers
         {
             var mappedCommand = command as PlaceRobot;
 
+            var isPositionOccupiedByWall = gridData.Any(x => x.Position.X == mappedCommand.Position.X && x.Position.Y == mappedCommand.Position.Y && x.IsWall);
+
+            if (isPositionOccupiedByWall)
+            {
+                return Responses.COORDINATES_ARE_OCCUPIED_BY_WALL;
+            }
+
             robot = new Robot { Position = mappedCommand.Position };
 
             return string.Empty;
-
         }
     }
 }
